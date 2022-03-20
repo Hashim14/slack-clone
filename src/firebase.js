1d360feffc5a89104c9e65f89a31a48876b5c03f
Hashim14 <45725764+Hashim14@users.noreply.github.com>
2022-03-20 17:10:20 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCfUGO4KcxF05E8on-alUlifKJOwDFF3tI",
+    authDomain: "slack-clone-ada80.firebaseapp.com",
+    projectId: "slack-clone-ada80",
+    storageBucket: "slack-clone-ada80.appspot.com",
+    messagingSenderId: "20036014513",
+    appId: "1:20036014513:web:e57d077d375f0b0d016d90",
+    measurementId: "G-DCFB6S810P"
+  };
